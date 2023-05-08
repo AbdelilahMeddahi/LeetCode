15f62172aa15ff435f6064b721b11f6bf9469271
abdelilah <ma.meddahi@esi-sba.dz>
2023-05-08 10:49:24 +0100

    container with most water

--- Medium/container_with_most_water.py
+++ Medium/container_with_most_water.py
@@ -1,13 +1,22 @@
+test_case = [4,2,0,3,2,5,2,5]
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        if height == [1,1]:
-            return 1
-        i , j = 0, len(height)-1
-        surface = 0 
-        while i < j:
-            surface = max(surface,min(height[i],height[j])*(j-i))
-            if height[i] < height[j]:
-                i+=1
-            else: 
-                j-=1
-        return surface
+    def trap(self, height) -> int:
+        if not height:
+            return 0
+        trapped_water=0
+        left = 0
+        right = len(height)-1
+        left_max = height[left]
+        right_max = height[right] 
+        while left < right:
+            if height[left]<height[right]:
+                left+=1
+                left_max = max(height[left],left_max)
+                trapped_water = left_max - height[left]
+            else:
+                right -= 1
+                right_max = max( height[right],right_max)
+                trapped_water += right_max - height[right]
+        return trapped_water
+s = Solution() 
+print(s.trap(height = test_case))
