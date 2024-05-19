176c670fea53c39ecec1b1ad65a3ea99dc3ff274
Abdelilah Meddahi <95318113+AbdelilahMeddahi@users.noreply.github.com>
2024-05-20 00:51:10 +0200

    Create XOR_operation_in_array.py

---
+++ Easy/XOR_operation_in_array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        nums = []
+        result = 0
+        for  i in range(n):
+            nums.append(start + 2 * i)
+            result ^= nums[i]
+        return result
