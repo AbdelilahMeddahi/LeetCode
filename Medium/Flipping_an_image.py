4996951b2f0e20c25c68ec7273d411e42e651698
Abdelilah Meddahi <95318113+AbdelilahMeddahi@users.noreply.github.com>
2024-06-03 14:37:41 +0200

    Create Flipping_an_image.py

---
+++ Medium/Flipping_an_image.py
@@ -0,0 +1,5 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        for i in range(len(image)):
+            image[i] = [int(not j) for j in image[i]][::-1]
+        return image
